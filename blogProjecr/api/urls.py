1b37032e61b9e40b33e46b8086bf4bfbac1929d6
Tanvir Rahman <trahman@paradynamix.com>
2021-04-14 23:51:47 +0600

    Authentication implementation

--- blogProjecr/api/urls.py
+++ blogProjecr/api/urls.py
@@ -1,13 +1,14 @@
 from django.db import router
 from django.urls import path, include
-from .views import ArticleViewSet
+from .views import ArticleViewSet, UserViewSet
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='articles')
+router.register('users', UserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('api/', include(router.urls))
     #path('articles/', artical_list),
     #path('articles/<int:pk>/', article_details),
     #path('articles/', ArticleList.as_view()),
--- blogProjecr/api/views.py
+++ blogProjecr/api/views.py
@@ -1,22 +1,18 @@
-from datetime import date
-from django.http.response import Http404
-from django.shortcuts import render, HttpResponse
-from rest_framework import serializers
-from rest_framework.serializers import Serializer
 from .models import Article
-from .serializers import ArticleSerializer
-from django.http import JsonResponse
-from rest_framework.parsers import JSONParser
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import APIView
-from rest_framework import generics
-from rest_framework import mixins
+from .serializers import ArticleSerializer, UserSerializer
 from rest_framework import viewsets
-from django.shortcuts import get_object_or_404
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
 
 # Using 'Model view set'
 class ArticleViewSet(viewsets.ModelViewSet):
     queryset = Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
