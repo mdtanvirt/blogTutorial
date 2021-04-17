3b23cea32d5f55d0f533fd95b64c4c361f917e73
Tanvir Rahman <trahman@paradynamix.com>
2021-04-17 16:36:47 +0600

    react class and functional component example

---
+++ blogProjecr/frontEnd/blog/src/components/MyClass.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class MyClass extends Component { 
+    render() {
+        return(
+            <h1>We are inside class components</h1>
+            )
+    }
+}
+
+export default MyClass;
